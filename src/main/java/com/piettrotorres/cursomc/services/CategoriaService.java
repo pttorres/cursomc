e39dd7b164e29efd6354b3b9cb01dbf589e5e9a3
Piettro Torres <piettro.alessandro@gmail.com>
2021-02-25 16:53:05 -0300

    Deleção de categoria e tratamento de exclusão

--- src/main/java/com/piettrotorres/cursomc/services/CategoriaService.java
+++ src/main/java/com/piettrotorres/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.piettrotorres.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.piettrotorres.cursomc.domain.Categoria;
 import com.piettrotorres.cursomc.repositories.CategoriaRepository;
+import com.piettrotorres.cursomc.services.exception.DataIntegrityException;
 import com.piettrotorres.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
@@ -29,5 +31,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produto associado ");
+		}
 	}
 }
