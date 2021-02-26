bd461881e5f1b23d99a294726ebdedc437676315
Piettro Torres <piettro.alessandro@gmail.com>
2021-02-26 11:47:46 -0300

    Inserção da validação de dados.

--- src/main/java/com/piettrotorres/cursomc/services/CategoriaService.java
+++ src/main/java/com/piettrotorres/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.piettrotorres.cursomc.domain.Categoria;
+import com.piettrotorres.cursomc.dto.CategoriaDTO;
 import com.piettrotorres.cursomc.repositories.CategoriaRepository;
 import com.piettrotorres.cursomc.services.exception.DataIntegrityException;
 import com.piettrotorres.cursomc.services.exception.ObjectNotFoundException;
@@ -54,6 +55,10 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 	
 }
